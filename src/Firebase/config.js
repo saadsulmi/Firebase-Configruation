b73875708524d91812328c924727cfc46e659845
saadsulmi <saadsulmi@gmail.com>
2023-06-25 19:42:10 +0530

    a configruation method for using firebase in our react project

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -4,12 +4,15 @@ import 'firebase/compat/auth'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2dios2Ld3XWaZVLcblsqijsBvJcBE7-w",
-    authDomain: "olx-clone-4f85c.firebaseapp.com",
-    projectId: "olx-clone-4f85c",
-    storageBucket: "olx-clone-4f85c.appspot.com",
-    messagingSenderId: "5532134693",
-    appId: "1:5532134693:web:96cdb5e9f1a96e5563becc"
+
+    //replace this config with one you get from the firebase;
+
+    apiKey: "AIzsSyA2dios2Ld3XWaZVLcblsqijsBvJcBE7-w",
+    authDomain: "olsx-clone-4f85c.firebaseapp.com",
+    projectId: "olx-clone-45f85c",
+    storageBucket: "olx-clone-45f85c.appspot.com",
+    messagingSenderId: "55321346493",
+    appId: "1:5532134693:web:96cd3b5e9f1a96e5563becc"
   };
 
 export default firebase.initializeApp(firebaseConfig);
